c984a71e735881ecd5c4824b21c58b2baf1a4551
Sondre2003 <264133@usn.no>
2026-04-03 17:37:34 +0200

    Bilde Zoom

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Nettsiden fungerer!");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+
+// finn ALLE bilder i cards
+const images = document.querySelectorAll(".card img");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+// lukk når du klikker
+modal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
